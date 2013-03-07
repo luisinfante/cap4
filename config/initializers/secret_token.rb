dce471f2217498eb9b04a6a90c5e0bbaafb9deed
luisinfante <luisinfanterivera.gmail.com>
2013-03-07 08:29:43 -0600

    inicio envios

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cap4::Application.config.secret_token = 'cc80aa9f080ed181ac53e25a22e1c849c6214de08ca100916ced7566c9e58b0ee089d336fea3cc46a1ff4793571628ef71ff20cbae191af45eb2c152a921217e'
